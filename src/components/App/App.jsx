0c50a39ddb1b7bcbb2c37cdc5f47e329e3329a07
Gregg Trunnell <gggtrunnell@gmail.com>
2025-01-23 16:36:24 -0600

    bootstrap 101

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,45 @@
 import './App.css';
-
-import Navigation from '../Navigation/Navigation';
+import Players from '../Players/Players';
+import Search from '../Search/Search';
+import AddPlayer from '../AddPlayer/AddPlayer';
+import { NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home';
+import MatchStats from '../MatchStats/MatchStats';
+import Navigation from '../Navigation/Navigation'
 
 function App() {
 
   return (
-    <div>
+    <div className="container p-3">
       <header>
         <h1>Super Heroes</h1>
+      <Navigation />
       </header>
-    <Navigation />
+      <main>
+      <Routes>
+        <Route
+          exact path="/"
+          element={ <Home /> }
+          />
+        <Route
+          exact path="/search"
+          element={ <Search /> }
+          />
+        <Route
+          exact path="/add-players"
+          element={ <AddPlayer /> }
+          />
+        <Route
+          exact path="/players"
+          element={ <Players /> }
+          />
+         <Route
+          exact path="/match-stats"
+          element={ <MatchStats /> }
+          />
+       </Routes>
+      </main>
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ function Home( ) {
   
   return (
      <div className='Home'>
-        <img src='https://www.clandestinecritic.co.uk/wp-content/uploads/2020/09/Marvel-Comics-Superheroes-Jim-Cheung-1024x819.jpg'/>
+        <img className="w-100" src='https://www.clandestinecritic.co.uk/wp-content/uploads/2020/09/Marvel-Comics-Superheroes-Jim-Cheung-1024x819.jpg'/>
     </div>
   );
   }
